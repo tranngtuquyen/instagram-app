665e16de3fda7437c99c709c4b58d5955efbcb98
Quyen Tran <tranngtuquyen@gmail.com>
2020-08-05 00:50:01 -0700

    Add Redux tools

--- client/src/store.js
+++ client/src/store.js
@@ -9,7 +9,7 @@ const initialState = {};
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware)
+  compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;

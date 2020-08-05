4e22d8274d41a356c43e511c8022bfeb8d139f51
Quyen Tran <tranngtuquyen@gmail.com>
2020-08-05 01:54:41 -0700

    Convert Navbar to functional component

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./navbar.css";
 import logo from "../../img/circle.png";
-import { Link, withRouter} from "react-router-dom";
+import { Link, withRouter, useLocation} from "react-router-dom";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import Search from "../search/Search";
@@ -12,122 +12,92 @@ import GalleryIcon from "./navbarIcons/GalleryIcon";
 import AvatarIcon from "./navbarIcons/AvatarIcon";
 import LogoutIcon from "./navbarIcons/LogoutIcon";
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      home: false,
-      explore: false,
-      gallery: false,
-      avatarIcon: false,
-    };
-    this.logoutUserHandle = this.logoutUserHandle.bind(this);
-  }
-
-  logoutUserHandle(e) {
-    e.preventDefault();
-    this.props.history.push("/");
-    this.props.logoutUser();
-  }
-
-  checkPathName(pathName, currentProfile) {
-    if (pathName == "/home") {
-      this.setState({
-        home: true,
-        explore: false,
-        gallery: false,
-        avatarIcon: false,
-      });
-    } else if (pathName == "/explore") {
-      this.setState({
-        home: false,
-        explore: true,
-        gallery: false,
-        avatarIcon: false,
-      });
-    } else if (pathName == "/gallery") {
-      console.log("hit gallery");
-      this.setState({
-        home: false,
-        explore: false,
-        gallery: true,
-        avatarIcon: false,
-      })
-    } else if (currentProfile && pathName == `/profile/${currentProfile.handle}`) {
-      this.setState({
-        home: false,
-        explore: false,
-        gallery: false,
-        avatarIcon: true,
-      })
-    } else {
-      this.setState({
-        home: false,
-        explore: false,
-        gallery: false,
-        avatarIcon: false,
-      })
-    }
-  }
-
-  componentDidMount() {
-    const pathName = this.props.history.location.pathname;
-    this.checkPathName(pathName, this.props.profile.currentProfile);
-  };
-  componentWillReceiveProps(nextProps) {
-    const pathName = nextProps.history.location.pathname;
-    this.checkPathName(pathName, this.props.profile.currentProfile);
-  }
+function Navbar (props) {
+  let location = useLocation();
+  let [home, setHome] = useState(false);
+  let [explore, setExplore] = useState(false);
+  let [gallery, setGallery] = useState(false);
+  let [avatarIcon, setAvatar] = useState(false);
   
-  render() {
-    // console.log(this.props.history);
-    const {isAuthenticated, user} = this.props.auth;
-    const { currentProfile } = this.props.profile;
-    
-    //Get real avatar of user from redux store
-    const avatar = currentProfile ? currentProfile.user.avatar : spinner;
-    const {home, explore, gallery, avatarIcon} = this.state;
-    //Put all navbar contents into variable "navbar" 
-    const navbar = (
-      <nav
-        className='navbar navbar-light navbar-expand-lg'
-        style={{ backgroundColor: "white", padding: "0px" }}
-      >
-        <div className='container'>
-          <Link className='navbar-brand' to=''>
-            <img className='logo-navbar' alt='Circle' src={logo} />
-          </Link>
+  useEffect(() => {
+    if (location.pathname === "/home") {
+      setHome(true);
+      setExplore(false);
+      setGallery(false);
+      setAvatar(false);
+    } else if (location.pathname === "/explore") {
+      setHome(false);
+      setExplore(true);
+      setGallery(false);
+      setAvatar(false);
+    } else if (location.pathname === "/gallery") {
+      setHome(false);
+      setExplore(false);
+      setGallery(true);
+      setAvatar(false);
+    } else if (props.profile.currentProfile && location.pathname === `/profile/${props.profile.currentProfile.handle}`) {
+      setHome(false);
+      setExplore(false);
+      setGallery(false);
+      setAvatar(true);
+    } else {
+      setHome(false);
+      setExplore(false);
+      setGallery(false);
+      setAvatar(false);
+    }
+  }, [location, props.profile.currentProfile]);
+  const logoutUserHandle = (e) => {
+    e.preventDefault();
+    props.logoutUser();
+  }
 
-          <Search/>
+  
+  const {isAuthenticated, user} = props.auth;
+  const { currentProfile } = props.profile;
+  //Get real avatar of user from redux store
+  const avatar = currentProfile ? currentProfile.user.avatar : spinner;
 
-          {/* Navbar Icons */}
-          <ul style={{ marginTop: "auto", marginBottom: "auto" }}>
-            <li>
-              <HomeIcon home={home}/>
-            </li>
-            <li>
-              <ExploreIcon explore={explore}/>
-            </li>
-            <li>
-              <GalleryIcon gallery={gallery}/>
-            </li>
-            <li>
-              <AvatarIcon currentProfile={currentProfile} avatarIcon={avatarIcon} avatar={avatar} />
-            </li>
-            <li>
-              <LogoutIcon logoutUserHandle={this.logoutUserHandle}/>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
+  //Put all navbar contents into variable "navbar" 
+  const navbar = (
+    <nav
+      className='navbar navbar-light navbar-expand-lg'
+      style={{ backgroundColor: "white", padding: "0px" }}
+    >
+      <div className='container'>
+        <Link className='navbar-brand' to=''>
+          <img className='logo-navbar' alt='Circle' src={logo} />
+        </Link>
+
+        <Search/>
+
+        {/* Navbar Icons */}
+        <ul style={{ marginTop: "auto", marginBottom: "auto" }}>
+          <li>
+            <HomeIcon home={home}/>
+          </li>
+          <li>
+            <ExploreIcon explore={explore}/>
+          </li>
+          <li>
+            <GalleryIcon gallery={gallery}/>
+          </li>
+          <li>
+            <AvatarIcon currentProfile={currentProfile} avatarIcon={avatarIcon} avatar={avatar} />
+          </li>
+          <li>
+            <LogoutIcon logoutUserHandle={(e) => logoutUserHandle(e)}/>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
     return (
       <div>
         {/* Check if user is isAuthenticated, if yes - show Navbar, if no - hide Navbar */}
         {isAuthenticated ? navbar : null}
       </div>
     );
-  }
 }
    
 const mapStateToProps = (state) => ({
